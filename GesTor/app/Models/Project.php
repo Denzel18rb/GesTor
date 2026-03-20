ade4976efad6d37a3e89ecbecfb20dcce89de99c
J4JVH <juanjesusvalentin2@gmail.com>
2026-03-19 21:16:57 -0500

    breeze agregado

--- GesTor/app/Models/Project.php
+++ GesTor/app/Models/Project.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,16 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $table = 'projects';
-    protected $primaryKey = 'id_project';
 
     protected $fillable = [
         'name',
-        'start_date',
         'end_date',
         'team_members_count',
         'tasks_count',
         'user_id'
     ];
-
-    public $timestamps = true;
 }
